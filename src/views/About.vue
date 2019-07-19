6a09228b07b375697c204b1ac9fec83f3f4f5c5a
yinheng <603729472@qq.com>
2019-07-19 17:14:45 +0800

    初始化项目

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img src="../assets/kurumi/white-bride.png" alt />
+  </div>
+</template>
+
+<script></script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  img {
+    max-width: 100%;
+  }
+}
+</style>
