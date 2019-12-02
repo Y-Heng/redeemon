40405b9e1149883af8b3622b3664c40839d6e205
yinheng <603729472@qq.com>
2019-12-03 02:44:12 +0800

    跟换背景图

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home" @click="$router.push('/')">
-    <img src="../assets/kurumi/white-bride.png" alt />
   </div>
 </template>
 
@@ -8,9 +7,9 @@
 .home {
   width: 100%;
   height: 100%;
-  img {
-    width: 100%;
-    max-width: 100%;
-  }
+  background: url('../assets/kurumi/white-bride.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home" @click="$router.push('/about')">
-    <img src="../assets/kurumi/block-bride.png" alt />
   </div>
 </template>
 
@@ -8,9 +7,9 @@
 .home {
   width: 100%;
   height: 100%;
-  img {
-    width: 100%;
-    max-width: 100%;
-  }
+  background: url('../assets/kurumi/block-bride.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 }
 </style>
